ad6018a08f84b85f7e2988f12670580f893ac234
Chris Dickson <70047029+DicksonChris@users.noreply.github.com>
2022-05-27 20:14:46 -0500

    Removed duplicated code

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,16 +1,14 @@
-import ContactForm from '../components/ContactForm'
-
-const ContactPage = () => {
-    return <ContactForm />
-}
-=======
 import { Container } from "reactstrap";
 import SubHeader from "../components/SubHeader";
+import ContactForm from '../components/ContactForm';
 
 const ContactPage = () => {
   return (
     <Container>
-      <SubHeader current="Contact" />
+      <SubHeader current='Contact' />
+      <ContactForm />
     </Container>
   );
 };
+
+export default ContactPage;

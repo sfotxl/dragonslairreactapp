d334f56c76e5fc7056f95c0d825acabd0de00eb6
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-05-30 13:29:51 -0500

    Makes CSS more specific and changes styling on subheader to be more legible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
-import { FaDrumstickBite } from "react-icons/fa";
+import { FaDrumstickBite } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -8,34 +8,66 @@ const Footer = () => {
       <Container>
         <Row>
           <Col xs={{ size: 2, offset: 1 }} sm='3'>
-            <h5>Links</h5>
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/'>
-              <i className='fa fa-home fa-lg' /> <p className='footer-links '>Home</p>
+            <h3 className='footer-title'>Links</h3>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/'
+            >
+              <i className='fa fa-home fa-lg' />{' '}
+              <p className='footer-links '>Home</p>
             </Link>
 
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/about'>
-              <i className='fa fa-address-card fa-lg' /><p className='footer-links '>About</p>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/about'
+            >
+              <i className='fa fa-address-card fa-lg' />
+              <p className='footer-links '>About</p>
             </Link>
 
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/products'>
-              <i className='fa fa-lg'><FaDrumstickBite/></i> <p className='footer-links '>Products</p>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/products'
+            >
+              <i className='fa fa-lg'>
+                <FaDrumstickBite />
+              </i>{' '}
+              <p className='footer-links '>Products</p>
             </Link>
 
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/health'>
-              <i className='fa fa-heartbeat fa-lg' /> <p className='footer-links '>Health</p>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/health'
+            >
+              <i className='fa fa-heartbeat fa-lg' />{' '}
+              <p className='footer-links '>Health</p>
             </Link>
 
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/vendor'>
-              <i className='fa fa-usd fa-lg' /> <p className='footer-links '>Vendor Info</p>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/vendor'
+            >
+              <i className='fa fa-usd fa-lg' />{' '}
+              <p className='footer-links '>Vendor Info</p>
             </Link>
 
-            <Link role='button' className='btn btn-link text-decoration-none ' to='/contact'>
-              <i className='fa fa-address-book fa-lg' /> <p className='footer-links '>Contact Us</p>
+            <Link
+              role='button'
+              className='btn btn-link text-decoration-none '
+              to='/contact'
+            >
+              <i className='fa fa-address-book fa-lg' />{' '}
+              <p className='footer-links '>Contact Us</p>
             </Link>
           </Col>
 
           <Col xs='6' sm='3' className='text-center'>
-            <h5>Social</h5>
+            <h3 className='footer-title'>Social</h3>
             <a
               className='btn btn-social-icon btn-instagram '
               href='http://instagram.com/'
@@ -51,8 +83,14 @@ const Footer = () => {
           </Col>
 
           <Col sm='4' className='text-center'>
-            <a role='button' className='btn btn-link text-decoration-none' href='tel:+12065551234'>
-              <i className='fa fa-phone' /> <p className='footer-links'>1-206-555-1234</p>
+            <h3 className='footer-title'>Contact</h3>
+            <a
+              role='button'
+              className='btn btn-link text-decoration-none'
+              href='tel:+12065551234'
+            >
+              <i className='fa fa-phone' />{' '}
+              <p className='footer-links'>1-206-555-1234</p>
             </a>
             <br />
             <a
@@ -60,7 +98,8 @@ const Footer = () => {
               className='btn btn-link text-decoration-none'
               href='mailto:notreal@notreal.co'
             >
-              <i className='fa fa-envelope-o' /> <p className='footer-links'>emailhere@gmail.com</p>
+              <i className='fa fa-envelope-o' />{' '}
+              <p className='footer-links'>emailhere@gmail.com</p>
             </a>
           </Col>
         </Row>

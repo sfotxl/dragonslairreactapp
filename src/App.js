6f8219a5449e17e6a3840a9d6bb57b608eb486c6
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-05-30 16:08:23 -0500

    Adds PageContainer to ensure correct page layout
    Adds PageContainer component, wraps entire page (Header, Content, Footer). This Component ensures that the footer is at the bootom of the page, without being a 'fixed' footer.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
-import AboutPage from "./pages/AboutPage";
-import HealthPage from "./pages/HealthPage";
-import ContactPage from "./pages/ContactPage";
-import VendorPage from "./pages/VendorPage";
-
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+import AboutPage from './pages/AboutPage';
+import HealthPage from './pages/HealthPage';
+import ContactPage from './pages/ContactPage';
+import VendorPage from './pages/VendorPage';
+import PageContainer from './components/PageContainer';
 
 function App() {
   return (
-        <div className="App bg-dark text-light">
-        <Header />
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-                <Route path="/products" element={<ProductPage />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/health" element={<HealthPage />} />
-                <Route path="/vendor" element={<VendorPage />} />
-                <Route path="/contact" element={<ContactPage />} />
-          </Routes>
-        <Footer />
-        </div>
+    <div className='App bg-dark text-light'>
+      <PageContainer>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/products' element={<ProductPage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/health' element={<HealthPage />} />
+          <Route path='/vendor' element={<VendorPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
+      </PageContainer>
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/PageContainer.js
@@ -0,0 +1,43 @@
+import { useEffect, useState, useRef } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+/**
+ * Wraps app pages, handles header, footer, and page height. 
+ * Ensures the footer is at the bottom of the page by adding a min-height to the main content.
+ * @props { node }  children  Child components
+ */
+const PageContainer = ({ children }) => {
+  const [headerHeight, setHeaderHeight] = useState(0)
+  const [footerHeight, setFooterHeight] = useState(0)
+  const headerRef = useRef(null)
+  const footerRef = useRef(null)
+
+  useEffect(() => {
+    setHeaderHeight(headerRef.current.offsetHeight)
+    setFooterHeight(footerRef.current.offsetHeight)
+  }, [])
+
+  const calcContentHeight = {
+    minHeight: `calc(100vh - ${headerHeight + footerHeight}px)`,
+  }
+
+  return (
+    <div className=''>
+      <span ref={headerRef} className='wrapper'>
+        <Header />
+      </span>
+      <div
+        className='w-100 mx-auto d-flex flex-column '
+        style={calcContentHeight}
+      >
+        {children}
+      </div>
+      <span ref={footerRef} className='wrapper'>
+        <Footer />
+      </span>
+    </div>
+  )
+}
+
+export default PageContainer

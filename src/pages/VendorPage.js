5e540e20bfe62175aec2b1197adea9732a9c5841
Kalani Man <kromelo@icloud.com>
2022-05-31 10:59:19 -0500

    Page layout, Dragon Image, Logo Text, Contact information

--- src/pages/VendorPage.js
+++ src/pages/VendorPage.js
@@ -1,10 +1,34 @@
-import { Container } from "reactstrap";
-import SubHeader from "../components/SubHeader";
+import { Container, Row, Col } from 'reactstrap';
+import SubHeader from '../components/SubHeader';
+import Map from '../features/maps/Map';
+import { ReactComponent as DragonLogoText } from '../assets/img/dragonslairlogo.svg';
+import redDragonImg from '../assets/img/redDragon.jpg';
 
 const VendorPage = () => {
   return (
     <Container>
-      <SubHeader current="Vendor" detail="Contact" />
+      <SubHeader current='Vendor' detail='Contact' />
+      <h1 className='secondCard mb-3'>Become a Vendor</h1>
+      <Row className="align-items-center">
+        <Col>
+          <img className='featuredImage' src={redDragonImg} width="100%"/>
+        </Col>
+        <Col>
+        <DragonLogoText />
+          <p>
+            Do you have a store, bar, or other place where you would like to
+            sell our fantastic products? Becoming a vendor is easy, and we will
+            work with you to set up a display. Reach out to Rob via e-mail or
+            phone.
+          </p>
+          <a href='mailto:dragonslairjerky@gmail.com'>
+            <i class='fa-solid fa-envelope fa-large'></i>
+          </a>
+          <a href='tel:+13109489318'>
+            <i class='fa-solid fa-phone'></i>
+          </a>
+        </Col>
+      </Row>
     </Container>
   );
 };

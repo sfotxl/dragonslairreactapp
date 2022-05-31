f2c887733570d2cd747d28ab6f9bcfbc546d904a
Kalani Man <kromelo@icloud.com>
2022-05-31 10:44:13 -0500

    Added Map to Contact Page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,7 @@
 import { Container } from "reactstrap";
 import SubHeader from "../components/SubHeader";
 import ContactForm from '../components/ContactForm';
+import Map from "../features/maps/Map";
 
 const ContactPage = () => {
   return (
@@ -8,6 +9,8 @@ const ContactPage = () => {
       <SubHeader current='Contact' />
       <h1 className='text-center text-white mb-3 fourthCard'>Contact Us</h1>
       <ContactForm />
+      <hr/>
+      <Map/>
     </Container>
   );
 };
